dbffd325486969a9807c5855701b31274b8e5776
eusebioaddsilva <eusebiodasilva417@gmail.com>
2023-02-03 00:56:20 +0000

    setup v2

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
+const { defineConfig } = require('cypress');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
 const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
 const createEsBuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
+
 module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
@@ -8,7 +10,7 @@ module.exports = defineConfig({
       const bundler = createBundler({
         plugins: [createEsBuildPlugin(config)],
       });
-      on('file: preprocessor', bundler);
+      on('file:preprocessor', bundler);
       await addCucumberPreprocessorPlugin(on, config);
       return config;
     },
